41ef062e17118a4a4efdf2b551bb5ffe15b93936
Kanimozhi-E <kanimozhi.e2024it@sece.ac.in>
2025-12-05 07:28:08 +0530

    Add files via upload

--- func.js
+++ func.js
@@ -68,3 +68,32 @@ a=[12,34,45,415,676,78]
 result= a.indexOf(415)
 console.log('the result',result)
 console.log('the arr is',a)
+
+const a=(s,p)=>{
+   
+    s+p
+    return p-s
+}
+let b=a(3,4)
+console.log(b)
+
+p=[3,2,4];
+ const p1=a.map((d)=>{return d*2})
+ console.log(p1)
+// arr=[];
+// for (var i=0;i<a.length;i++){ 
+//      arr.push(a[i]*2)
+// }
+// console.log(arr)
+
+let nums5 = [10,1,2,4,5,12, 20, 30, 40];
+ const a6=nums5.filter((d)=>{ return d%2==0})
+// const a=[]
+// for(let d of nums)
+// {
+//    if(d%2==0)
+//    {
+//     a.push(d)
+//    }
+// }
+  console.log(a6)
